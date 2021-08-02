f602ab1aec67092d33e053c0a9f1ae61fcebd7ab
Sneha Chauhan <58876232+Sneha206-sneha@users.noreply.github.com>
2021-08-02 19:30:39 +0530

    Update script.js

--- assests/js/script.js
+++ assests/js/script.js
@@ -98,10 +98,7 @@ document.onkeydown = function(e) {
 }
 
 // Start of Tawk.to Live Chat
-<!--Start of Tawk.to Script-->
-<div id='tawk_6107ec94d6e7610a49ae2c7b'></div>
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
+
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date(); Tawk_API.embedded='tawk_6107ec94d6e7610a49ae2c7b';
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -110,8 +107,6 @@ s1.src='https://embed.tawk.to/6107ec94d6e7610a49ae2c7b/1fc3i63vh';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);})();
-</script>
-<!--End of Tawk.to Script-->
 
 
 // End of Tawk.to Live Chat
